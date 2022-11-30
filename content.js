608f47ecbb56b168bf262917b53197aa486a9cb7
ZemyCode <69816894+ZemyCode@users.noreply.github.com>
2022-11-29 17:22:26 -0800

    v0.2.8 using interval as bug fix for now

--- content.js
+++ content.js
@@ -1,8 +1,6 @@
 const promotedSvg =
   "M19.498 3h-15c-1.381 0-2.5 1.12-2.5 2.5v13c0 1.38 1.119 2.5 2.5 2.5h15c1.381 0 2.5-1.12 2.5-2.5v-13c0-1.38-1.119-2.5-2.5-2.5zm-3.502 12h-2v-3.59l-5.293 5.3-1.414-1.42L12.581 10H8.996V8h7v7z";
 const adDiv = "div[data-testid=placementTracking]";
-const timelineDiv = 'div[aria-label="Timeline: Your Home Timeline"]';
-var timeline;
 
 function hidePromotedTweets() {
   document.querySelectorAll(adDiv).forEach((element) => {
@@ -12,15 +10,19 @@ function hidePromotedTweets() {
   });
 }
 
+//attempted solution for non LCP browsers
+const deletionInterval = setInterval(hidePromotedTweets(), 100);
+
 //check for promoted tweets on page load
 document.addEventListener("load", () => {
-  timeline = document.querySelector(timelineDiv);
   hidePromotedTweets();
-  startInterval();
 });
 
-//check for promoted tweets when user scrolls (temporarily disabled since setInterval is being tested)
-// document.addEventListener("scroll", () => hidePromotedTweets());
+//check for promoted tweets when user scrolls and stop the interval
+document.addEventListener("scroll", () => {
+  clearInterval(deletionInterval);
+  hidePromotedTweets();
+});
 
 //handle promoted tweets which load after the page
 const observer = new PerformanceObserver(() => {
@@ -29,10 +31,3 @@ const observer = new PerformanceObserver(() => {
 
 //LCP only supported in Chromium based browsers, not Firefox or Safari
 observer.observe({ type: "largest-contentful-paint", buffered: true });
-
-//attempted solution for non LCP browsers
-function startInterval() {
-  timeline.setInterval(() => {
-    hidePromotedTweets();
-  }, 1000);
-}
